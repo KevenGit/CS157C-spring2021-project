bb9817bb2c2bece60799632727223408b4b50600
Bikalp Timalsina <48162138+Youngbikalp@users.noreply.github.com>
2021-04-23 22:08:23 -0700

    initial commit

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,71 +1,72 @@
 const Article = require("../models/article");
 
-const article_search = (req, res) => { //Search Article
-    console.log(req.query.abstract);
-    Article.find({abstract: { "$regex": req.query.abstract }})
-    .then(results => {
-        res.send(results)
-    });
-    
-}
+const article_search = (req, res) => {
+  //Search Article
+  console.log(req.query.abstract);
+  Article.find({ abstract: { $regex: req.query.abstract } }).then((results) => {
+    res.send(results);
+  });
+};
 
- const article_delete = (req, res) => { //Delete Article
-    console.log(req.query.abstract);
-    Article.findOneAndDelete({abstract: { "$regex": req.query.abstract }})
-    .then(results => {
-        console.log(results)
-    });
-}
-
-const article_create = (req, res) => { //Insert New Articles
-    const doc = req.body;
-
-    const obj = {
-        abstract: doc.abstract,
-        web_url: doc.web_url,
-        snippet: doc.snippet,
-        lead_paragraph: doc.lead_paragraph,
-        source: doc.source,
-
-        headline: {
-            main: doc.headline_main,
-            kicker: doc.headline_kicker,
-            print_headline: doc.headline_print_headline
-        },
-
-        keywords: [],
-
-        pub_date: Date.now().toString,
-        document_type: doc.document_type,
-        news_desk: doc.news_desk,
-        section_name: doc.section_name,
-        subsection_name: doc.subsection_name,
-
-        byline: {
-            original: doc.byline.original,
-            organization: doc.byline.organization
-        },
-        type_of_material: doc.type_of_material,
-        word_count: doc.word_count
+const article_delete = (req, res) => {
+  //Delete Article
+  console.log(req.query.abstract);
+  Article.findOneAndDelete({ abstract: { $regex: req.query.abstract } }).then(
+    (results) => {
+      console.log(results);
     }
+  );
+};
 
-    const article = new Article(obj);
-    article.save()
-    .then(result => {
-        res.send(result);
+const article_create = (req, res) => {
+  //Insert New Articles
+  const doc = req.body;
+
+  const obj = {
+    abstract: doc.abstract,
+    web_url: doc.web_url,
+    snippet: doc.snippet,
+    lead_paragraph: doc.lead_paragraph,
+    source: doc.source,
+
+    headline: {
+      main: doc.headline_main,
+      kicker: doc.headline_kicker,
+      print_headline: doc.headline_print_headline,
+    },
+
+    keywords: [],
+
+    pub_date: Date.now().toString,
+    document_type: doc.document_type,
+    news_desk: doc.news_desk,
+    section_name: doc.section_name,
+    subsection_name: doc.subsection_name,
+
+    byline: {
+      original: doc.byline.original,
+      organization: doc.byline.organization,
+    },
+    type_of_material: doc.type_of_material,
+    word_count: doc.word_count,
+  };
+
+  const article = new Article(obj);
+  article
+    .save()
+    .then((result) => {
+      res.send(result);
     })
-    .catch(err => {
-        console.error(err);
-    });   
-}
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-const article_bookmark = (req, res) => {
-    
-}
+const article_bookmark = (req, res) => {};
 
 module.exports = {
-    article_create,
-    article_delete,
-    article_search,
-    article_bookmark,
-}
+  article_create,
+  article_delete,
+  article_search,
+  article_bookmark,
+};
