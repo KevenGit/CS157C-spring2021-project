7ba29eb3e6ca053efaaf991dc86dc334314ed24d
Bikalp Timalsina <48162138+Youngbikalp@users.noreply.github.com>
2021-04-23 18:18:48 -0700

    FrontEnd and some backend

--- Routes/articleRoutes.js
+++ Routes/articleRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const articleController = require('../controllers/articleController');
+const articleController = require("../controllers/articleController");
 
-router.get('/', articleController.articles_get);
-router.post('/create', articleController.article_create);
-router.get('/search', articleController.article_search);
-// router.delete('/:id', articleController.article_delete);
-// router.get('/:id', articleController.article_get);
-// router.put('/:id', articleController.article_update);
+router.get("/", articleController.articles_get);
+router.post("/create", articleController.article_create);
+//router.get("/search", articleController.article_search);
+router.delete("/:id", articleController.article_delete);
+router.get("/:id", articleController.article_get);
+router.put("/update", articleController.article_update);
 
 module.exports = router;
